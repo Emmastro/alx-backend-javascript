618de4584377a805a14bf98dd92cde1810308f16
Emmanuel Murairi <emurairi18@alastudents.org>
2021-08-20 20:11:00 +0400

    fix data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,22 @@
 /* eslint-disable */
+const cleanSet = (set, startString) => {
+  const str = [];
+
+  if (
+    typeof set !== 'object' ||
+    typeof startString !== 'string' ||
+    startString.length === 0
+  ) {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      str.push(item.slice(startString.length));
+    }
+  }
+
+  return str.join('-');
+};
+
+export default cleanSet;
